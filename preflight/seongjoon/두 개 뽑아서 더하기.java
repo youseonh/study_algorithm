9e371acbcc84ab1d205d9d749fdb17b3a773f268
Meringue-KIm <dd8873@naver.com>
2023-02-13 18:05:45 +0900

    [성준] 0주차 - 프로그래머스 / 두 개 뽑아서 더하기

---
+++ preflight/seongjoon/두 개 뽑아서 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class 두 개 뽑아서 더하기 {
+
+    public List<Integer> solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        return list;
+    }
+
+}
