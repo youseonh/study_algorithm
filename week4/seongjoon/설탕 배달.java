5cf76e11602bce400199abf23aedd219579d8e4d
Meringue-KIm <dd8873@naver.com>
2023-03-13 22:02:12 +0900

    [성준] 4주차 - 설탕 배달

---
+++ week4/seongjoon/설탕 배달.java
@@ -0,0 +1,21 @@
+package study.week5;
+
+import java.util.Scanner;
+
+public class 설탕 배달 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        } else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        } else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        } else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
+    }
+
+}
