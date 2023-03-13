33a66deee3e30f00200f172f38383a023fe29c95
youseon <dbtjs0221@gmail.com>
2023-03-13 13:23:17 +0900

    [유선] 3주차 - 하노이의 탑 추가

---
+++ week3/youseon/하노이의 탑.py
@@ -0,0 +1,15 @@
+# 하노이의 탑
+def solution(n):
+    answer = []
+    
+    def hanoi(src, tgt, inter, n):
+        if n == 1:
+            answer.append([src, tgt])
+        else:
+            hanoi(src,inter,tgt,n-1)
+            hanoi(src,tgt,inter,1)
+            hanoi(inter,tgt,src,n-1)
+            
+    hanoi(1,3,2,n)
+    
+    return answer
