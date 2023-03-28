80630bfcd298f1e3586827ac7ab5ea999c12c3e9
Meringue-KIm <dd8873@naver.com>
2023-03-28 18:43:10 +0900

    [성준] 6주차 - 바닥 장식

---
+++ week6/seongjoon/바닥 장식.java
@@ -0,0 +1,49 @@
+package study.week6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 바닥 장식 {
+
+    public static void main(String[] args) throws Exception {
+        new Q3().solution();
+    }
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        char[][] arr = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = row.charAt(j);
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int tmp = 0;
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == '|') {
+                    tmp = 0;
+                } else if (++tmp == 1) {
+                    cnt++;
+                }
+            }
+        }
+        for (int j = 0; j < m; j++) {
+            int tmp = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i][j] == '-') {
+                    tmp = 0;
+                } else if (++tmp == 1) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+
+}
