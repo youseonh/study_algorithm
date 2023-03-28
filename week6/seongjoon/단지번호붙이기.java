c34dbdd0ccbb8cb8022d983b651b1d58542b5c89
Meringue-KIm <dd8873@naver.com>
2023-03-28 18:42:56 +0900

    [성준] 6주차 - 단지번호붙이기

---
+++ week6/seongjoon/단지번호붙이기.java
@@ -0,0 +1,84 @@
+package study.week6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 단지번호붙이기 {
+
+    static int arr[][];
+    static boolean visit[][];
+    static int dirX[] = {0, 0, -1, 1};
+    static int dirY[] = {-1, 1, 0, 0};
+
+    static int count = 0, number = 0;
+    static int nowX, nowY, N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<Integer> list = new ArrayList<>();
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        visit = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Character.getNumericValue(str.charAt(j));
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+
+                if (visit[i][j] == false && arr[i][j] == 1) {
+                    count = 0;
+                    number++;
+                    DFS(i, j);
+                    list.add(count);
+                }
+
+            }
+        }
+
+        Collections.sort(list);
+        bw.append(number + "\n");
+        for (int num : list) {
+            bw.append(num + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    } // End of main
+
+    static void DFS(int x, int y) {
+        visit[x][y] = true;
+        arr[x][y] = number;
+        count++;
+
+        for (int i = 0; i < 4; i++) {
+            nowX = dirX[i] + x;
+            nowY = dirY[i] + y;
+
+            if (Range_check() && visit[nowX][nowY] == false && arr[nowX][nowY] == 1) {
+                visit[nowX][nowY] = true;
+                arr[nowX][nowY] = number;
+
+                DFS(nowX, nowY);
+            }
+        }
+
+    } // End of DFS
+
+    static boolean Range_check() {
+        return (nowX >= 0 && nowX < N && nowY >= 0 && nowY < N);
+    } // End of Range_check
+} // End of class
+
